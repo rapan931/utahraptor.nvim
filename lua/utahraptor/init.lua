7b64d5ceba3f7abbfe1aceeec21e5ce2059cd915
rapan931 <24415677+rapan931@users.noreply.github.com>
2022-08-07 00:20:09 +0900

    fix: Adjust interval, add stop flag and consider window ID

--- lua/utahraptor/init.lua
+++ lua/utahraptor/init.lua
@@ -1,61 +1,56 @@
 local fn = vim.fn
 local api = vim.api
 
-local call_count = 0
-
-local function matchdelete(match_id)
-  if match_id ~= nil then
-    fn.matchdelete(match_id)
-  end
-end
-
-M = {}
-
 local function l_flash(wait_ms)
-  call_count = call_count + 1
-  local current_call_count = call_count
-
+  local win_id = fn.win_getid()
   local pattern = fn.getreg('/')
-  local last_en_chars = fn.matchstr(pattern, [[\\\+$]])
+  local last_backslash_chars = fn.matchstr(pattern, [[\\\+$]])
 
   local end_cars
-  if #last_en_chars == 0 then
+  if #last_backslash_chars == 0 then
     end_cars = [[\)]]
   elseif #pattern == 1 then
     -- /\<CR>
     end_cars = [[\\)]]
-  elseif #last_en_chars % 2 == 0 then
+  elseif #last_backslash_chars % 2 == 0 then
     -- /hogehoge\\<CR>
-    -- /huga\\\\<CR>
+    -- /hogehoge\\\\<CR>
     end_cars = [[\)]]
   else
     -- /hogehoge\<CR>
-    -- /huga\\\<CR>
+    -- /hogehoge\\\<CR>
     end_cars = [[\\)]]
   end
 
   local l, c = unpack(vim.list_slice(fn.getpos('.'), 2, 3))
   local l_pattern = [[\%]] .. l .. [[l]]
   local c_pattern = [[\%]] .. c .. [[c]]
+  local search_pattern = l_pattern .. c_pattern .. [[\(]] .. pattern .. end_cars
 
-  local match_pattern_id = fn.matchadd('CursorIM', l_pattern .. c_pattern .. [[\(]] .. pattern .. end_cars, 100)
-  local match_cursor_id = fn.matchadd('Cursor', l_pattern .. c_pattern, 101)
+  local match_pattern_id = fn.matchadd('ErrorMsg', search_pattern, 100, -1, { window = win_id })
+  local match_cursor_id = fn.matchadd('Cursor', l_pattern .. c_pattern, 101, -1, { window = win_id })
 
-  vim.cmd('redraw')
+  -- vim.cmd('redraw')
 
   local timer = vim.loop.new_timer()
   local i = 1
-  timer:start(1, 25, vim.schedule_wrap(function()
+  local stopped = false
+  local interval = 42
+  timer:start(1, interval, vim.schedule_wrap(function()
     local ll, cc = unpack(vim.list_slice(fn.getpos('.'), 2, 3))
-    if i * 20  > wait_ms or (ll ~= l or cc ~= c)then
-      matchdelete(match_pattern_id)
-      matchdelete(match_cursor_id)
-      timer:close()  -- Always close handles to avoid leaks.
+    if i * interval  > wait_ms or (ll ~= l or cc ~= c) or fn.win_getid() ~= win_id then
+      if not stopped then
+        stopped = true
+        timer:close()
+        fn.matchdelete(match_pattern_id, win_id)
+        fn.matchdelete(match_cursor_id, win_id)
+      end
     end
     i = i + 1
   end))
 end
 
+M = {}
 M.flash = function()
   local ok, result = pcall(l_flash, 1000)
 
